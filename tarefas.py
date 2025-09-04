f176af283c69958b4dc1e91173ba663b5392ac98
Giovanna Lima de Souza <giovanna.lima1@aluno.ifsp.edu.br>
2025-09-04 17:19:56 -0300

    correções finais

--- tarefas.py
+++ tarefas.py
@@ -1,44 +1,47 @@
 import sqlite3
 
-conn = sqlite3.connect('tarefas.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-        CREATE TABLE IF NOT EXISTS tarefas(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        titulo TEXT NOT NULL,
-        concluida INTEGER DEFAULT 0
-        );
-    '''
-)
-
-
-
-conn.commit()
+def criarTabela():
+    conn = sqlite3.connect('tarefas.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tarefas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            concluida INTEGER DEFAULT 0
+            );
+        '''
+    )
+    conn.commit()
+    conn.close()
 
 
 def adicionarTarefa(titulo):
+    conn = sqlite3.connect('tarefas.db')
+    cursor = conn.cursor()
     cursor.execute('''
         INSERT INTO tarefas (titulo, concluida) VALUES (?, ?); 
     '''
     , (titulo, 0)
     )
 
-    cursor.commit()
+    conn.commit()
     conn.close()
 
 def listarTarefas():
+    conn = sqlite3.connect('tarefas.db')
+    cursor = conn.cursor()
     cursor.execute(
         '''
         SELECT * FROM tarefas;
 '''
     )
     tarefas = cursor.fetchall()
-    tarefaDict = [dict(u) for u in tarefas]
     conn.close()
-    return tarefaDict
+    return [{"id": t[0], "titulo": t[1], "concluida": t[2]} for t in tarefas] 
 
 def marcarComoConcluida(id):
+    conn = sqlite3.connect('tarefas.db')
+    cursor = conn.cursor()
     cursor.execute(
         '''
             UPDATE tarefas
@@ -48,8 +51,11 @@ def marcarComoConcluida(id):
         , (id, )
     )
     conn.commit()
+    conn.close()
 
 def removerTarefa(id):
+    conn = sqlite3.connect('tarefas.db')
+    cursor = conn.cursor()
     cursor.execute(
         '''
             DELETE FROM tarefas
@@ -58,8 +64,10 @@ def removerTarefa(id):
         , (id, )
     )
     conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
+    criarTabela()
     while True:
         print("\n --- MENU ---" \
         "\n1 - Adicionar tarefa" \
@@ -74,7 +82,14 @@ if __name__ == "__main__":
             titulo = input("Digite o título da tarefa: ")
             adicionarTarefa(titulo)
         elif opcao == "2":
-            listarTarefas()
+            tarefas = listarTarefas()
+            if not tarefas:
+                print("Nenhuma tarefa encontrada.")
+            else:
+                print("\n--- LISTA DE TAREFAS ---")
+                for t in tarefas:
+                    print(f"{t['id']} - {t['titulo']} {t['concluida']}")
+            input("\nPressione ENTER para voltar ao menu.")
         elif opcao == "3":
             idTarefa = int(input("Digite o ID da tarefa: "))
             marcarComoConcluida(idTarefa)
